9b0246410bb72cdab92ad51d781cb563bed64a4f
close852 <jiwoo.choi@kccc.org>
2023-02-15 00:51:37 +0900

    loginUser parsing 처리 필요

--- frontend/src/recoils/User/state.tsx
+++ frontend/src/recoils/User/state.tsx
@@ -1,29 +1,13 @@
 import {atom, AtomEffect} from "recoil";
 import {getStorage, setStorage} from "utils/SecureStorage";
 import {recoilPersist} from "recoil-persist";
-const {persistAtom} = recoilPersist();
+import {localStorageEffect} from "@recoils/consonants";
 
-const localStorageEffect =
-  (key: string) =>
-  ({setSelf, onSet}: any) => {
-    const savedValue = getStorage(key);
-    if (savedValue !== null) {
-      setSelf(savedValue);
-    }
-    onSet((newValue: any, _: any, isReset: boolean) => {
-      console.log("new Value : ", newValue);
-      setStorage(key, JSON.stringify(newValue));
-      console.log("newvalue, isReset : ", newValue, isReset);
-    });
-  };
+function isEmpty(str: string) {
+  return str == "" || str == undefined || str == null;
+}
 export const userState = atom({
   key: "userState",
-  default: getStorage("#user") || null,
+  default: isEmpty(getStorage("#user")) ? null : JSON.parse(getStorage("#user")),
   effects: [localStorageEffect("#user")],
-});
-
-export const userGoogleAuthState = atom({
-  key: "userUserState",
-  default: null,
-  effects_UNSTABLE: [persistAtom],
 });
