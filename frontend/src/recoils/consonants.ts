9e239fbe5dcdbaffee5bcbf89162ccbc71727744
close852 <jiwoo.choi@kccc.org>
2023-02-14 16:32:25 +0900

    Update consonants.ts

--- frontend/src/recoils/consonants.ts
+++ frontend/src/recoils/consonants.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const SERVER = "http://localhost:4000/";
+const SERVER = "http://localhost:4000" + "/api/v1";
 // const SERVER = "http://13.125.79.139/";
 
 export const api = axios.create({
