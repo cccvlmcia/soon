25fc115d0fa1f2513e891e76b1eb5acdf1ff9cd9
close852 <jiwoo.choi@kccc.org>
2023-02-15 15:50:28 +0900

    Update query.tsx
    options 위치 변경

--- frontend/src/recoils/user/query.tsx
+++ frontend/src/recoils/user/query.tsx
@@ -1,19 +1,8 @@
+import {options} from "@recoils/consonants";
 import {useQuery} from "react-query";
-const options = {
-  refetchOnWindowFocus: false,
-  retry: 0,
-  onSuccess: ({data}: any) => {
-    //api 호출 성공
-    console.log("onSuccess >>", data);
-  },
-  onError: (error: any) => {
-    //api 호출 실패
-    console.log("onError >> ", error.message);
-  },
-};
-/*
-export const sampleQuery = () => {
-  const {isLoading, isError, data, error} = useQuery("sampleQuery", sampleAxios, options);
+import {getAuthUser} from "./axios";
+
+export const getAuthUserQuery = () => {
+  const {isLoading, isError, data, error} = useQuery("getAuthUser", () => getAuthUser(), options);
   return {isLoading, isError, data: data?.data, error};
 };
-*/
