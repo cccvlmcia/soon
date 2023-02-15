851d4f7fb89a9c2a97d324ad5c00afb0a2c1aea4
close852 <jiwoo.choi@kccc.org>
2023-02-15 11:43:21 +0900

    EXCEPT_URL 추가

--- server/config/haqqaton.config.ts
+++ server/config/haqqaton.config.ts
@@ -3,5 +3,6 @@ import haqqaton from "./haqqaton.json";
 const isProd = process.env.NODE_ENV === "production";
 const isLocal = process.env.NODE_ENV === "local";
 const SERVER_URI = isProd ? haqqaton.server.prod : isLocal ? haqqaton.server.local : haqqaton.server.dev;
+const EXCEPT_URL = ["/auth/google/callback"];
 
-export {haqqaton, SERVER_URI};
+export {haqqaton, SERVER_URI, EXCEPT_URL};
