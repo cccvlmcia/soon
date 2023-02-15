633bad1250a57f2041e08e59e7feb448932544ab
close852 <jiwoo.choi@kccc.org>
2023-02-15 15:53:56 +0900

    user 가져오는 쿼리 수정

--- frontend/src/recoils/user/query.tsx
+++
@@ -1,8 +0,0 @@
-import {options} from "@recoils/consonants";
-import {useQuery} from "react-query";
-import {getAuthUser} from "./axios";
-
-export const getAuthUserQuery = () => {
-  const {isLoading, isError, data, error} = useQuery("getAuthUser", () => getAuthUser(), options);
-  return {isLoading, isError, data: data?.data, error};
-};
