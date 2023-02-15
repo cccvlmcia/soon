3cdf833fc6920bf1e7225be4c2f186c46e63e123
goberomsu <310o@naver.com>
2023-02-15 13:18:12 +0900

    Fix : axios process 잘못 삭제함

--- frontend/src/recoils/consonants.ts
+++ frontend/src/recoils/consonants.ts
@@ -9,3 +9,20 @@ export const server = axios.create({
   baseURL: "/",
   withCredentials: true,
 });
+
+export async function axiosProcess(caller: Function, isLogin = false) {
+  try {
+    return await caller();
+  } catch (err) {
+    console.error("err ... ", err);
+    if (isLogin) {
+      //refresh token 남아 있으면 갱신?!
+      const {data} = await server.post("/auth/refreshToken");
+      if (data == "USER_AUTHENTICATED") {
+        return await caller();
+      }
+    }
+    //FIXME: alert으로 알려주고 넘어가야하는지 확인
+    return null;
+  }
+}
