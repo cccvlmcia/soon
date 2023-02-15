bd237f68fee99f444a1a56e287db94bc09f96a9f
close852 <jiwoo.choi@kccc.org>
2023-02-16 02:44:49 +0900

    editUser 추가
    사용자 정보 수정 페이지 추가

--- frontend/src/recoils/user/axios.tsx
+++ frontend/src/recoils/user/axios.tsx
@@ -1,8 +1,11 @@
-import {api, server} from "@recoils/consonants";
+import {api, axiosProcess, server} from "@recoils/consonants";
 import {postUser} from "@recoils/types";
 
 export function postUserRegistAxios(userInfo: postUser) {
   return api.post("/user", userInfo);
+}
+export async function editUser(userid: number, userInfo: any) {
+  return await axiosProcess(async () => await api.put(`/user/${userid}`, userInfo), true);
 }
 export const getAuthUser = () => server.get("/auth/user");
 export const postLogout = () => server.post("/auth/logout");
