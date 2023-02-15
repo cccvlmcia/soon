b80bbd65326ee9ee6a936cedb706a922d5ff59c8
close852 <jiwoo.choi@kccc.org>
2023-02-15 16:02:30 +0900

    Update Login.tsx

--- frontend/src/components/pages/Login/Login.tsx
+++ frontend/src/components/pages/Login/Login.tsx
@@ -12,17 +12,11 @@ const Login = () => {
 
   const navigate = useNavigate();
   const handleLoginSuccess = async (code: string) => {
-
     const {
       data,
       data: {status, auth, user},
     } = await getGoogleInfoAxios(code);
-<<<<<<< HEAD
-    console.log("dat >>", data, status, auth, user);
-=======
 
-
->>>>>>> 42de39b192f51136c5af3cb3debed2fb37668378
     if (status == "REGISTER") {
       setGoogleAuth(data);
 
