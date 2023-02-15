50205515106ae20d334e0892f7e852582b778ac4
close852 <jiwoo.choi@kccc.org>
2023-02-15 09:58:22 +0900

    Update consonants.ts

--- frontend/src/recoils/consonants.ts
+++ frontend/src/recoils/consonants.ts
@@ -2,11 +2,11 @@ import axios from "axios";
 import {getStorage, setStorage} from "utils/SecureStorage";
 
 export const api = axios.create({
-  baseURL: "http://localhost:4000/api/v1",
+  baseURL: "/api/v1",
   withCredentials: true,
 });
 export const server = axios.create({
-  baseURL: "http://localhost:4000/",
+  baseURL: "/",
   withCredentials: true,
 });
 
