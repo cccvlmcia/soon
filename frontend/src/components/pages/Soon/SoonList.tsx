ce4669619e29044c128573e21c1497395c58e4de
close852 <jiwoo.choi@kccc.org>
2023-02-16 17:36:29 +0900

    Update SoonList.tsx
    SoonList 단순 조회 기능만

--- frontend/src/components/pages/Soon/SoonList.tsx
+++ frontend/src/components/pages/Soon/SoonList.tsx
@@ -15,8 +15,6 @@ export default function SoonList() {
   const loginUser: any = useRecoilValue(userState);
   const userid = loginUser?.userid || 1;
   const [soonlist, setSoonlist] = useState([]);
-  const [addOpen, setAddOpen] = useState(false);
-  const [delOpen, setDelOpen] = useState(false);
   const {isLoading, isError, data, error} = getSoonListQuery(userid);
   if (isLoading) {
     return <Loading />;
@@ -24,15 +22,11 @@ export default function SoonList() {
   if (isError) {
     return <Error error={error} />;
   }
-  const datalist: never[] = data;
   useEffect(() => {
-    if (datalist) {
-      listFunc();
+    if (data) {
+      setSoonlist(data);
     }
-  }, [data, setSoonlist]);
-  const listFunc = () => {
-    setSoonlist([...datalist]);
-  };
+  }, [data]);
   return (
     <Box>
       <Box sx={{display: "flex", flexDirection: "column", alignItems: "center", marginTop: "10px"}}>
@@ -41,102 +35,6 @@ export default function SoonList() {
         ))}
       </Box>
       {/*FIXME: 순원 추가/삭제는 관리자 권한! */}
-      {/* <Stack direction={"row"}>
-        <SoonAddButton sjid={userid} setSoonlist={setSoonlist} textFieldOpen={addOpen} setTextFieldOpen={setAddOpen} />
-        <SoonDeleteButton sjid={userid} setSoonlist={setSoonlist} textFieldOpen={delOpen} setTextFieldOpen={setDelOpen} />
-      </Stack> */}
-    </Box>
-  );
-}
-
-function SoonAddButton({
-  sjid,
-  setSoonlist,
-  textFieldOpen,
-  setTextFieldOpen,
-}: {
-  sjid: number;
-  setSoonlist: any;
-  textFieldOpen: boolean;
-  setTextFieldOpen: React.Dispatch<React.SetStateAction<boolean>>;
-}) {
-  const [text, setText] = useState("");
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setText(event.target.value);
-  };
-
-  const handleAdd = async () => {
-    const swid = Number(text);
-    const result = await api.post("/soon", {sjid, swid});
-    if (result) {
-      setSoonlist((list: any) => [...list, result.data]);
-    }
-  };
-
-  return (
-    <Box>
-      <Button
-        variant="contained"
-        color="primary"
-        sx={styles.soon.button}
-        onClick={() => {
-          setTextFieldOpen(!textFieldOpen);
-        }}>
-        +
-      </Button>
-      {/* FIXME: 순원추가 버튼 > Full-screen dialogs에서 전체 캠퍼스 사용자 중에 선택하는 걸로 */}
-      {textFieldOpen && (
-        <Box onClick={handleAdd}>
-          <TextField label="순원id를 입력해주세요" value={text} onChange={handleChange} />
-          <Button type="submit" variant="contained">
-            순원추가
-          </Button>
-        </Box>
-      )}
-    </Box>
-  );
-}
-
-function SoonDeleteButton({
-  sjid,
-  setSoonlist,
-  textFieldOpen,
-  setTextFieldOpen,
-}: {
-  sjid: number;
-  setSoonlist: any;
-  textFieldOpen: boolean;
-  setTextFieldOpen: React.Dispatch<React.SetStateAction<boolean>>;
-}) {
-  const [text, setText] = useState("");
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setText(event.target.value);
-  };
-  const handleDelete = async () => {
-    const swid = Number(text);
-    const result = await api.delete(`/soon/${sjid}/${swid}`);
-    if (result) {
-      setSoonlist((list: any) => list.filter((soon: any) => soon.swid != swid));
-    }
-  };
-
-  return (
-    <Box>
-      <Button
-        variant="contained"
-        color="primary"
-        sx={styles.soon.button}
-        onClick={() => {
-          setTextFieldOpen(!textFieldOpen);
-        }}>
-        -
-      </Button>
-      {textFieldOpen && (
-        <Box onClick={handleDelete}>
-          <TextField label="순원id를 입력해주세요" value={text} onChange={handleChange} />
-          <Button variant="contained">순원삭제</Button>
-        </Box>
-      )}
     </Box>
   );
 }
