d1fcf7e861c65646fa7f6fe3884dbf5230fda046
Asiject <oj_only@naver.com>
2023-02-16 11:34:23 +0900

    register, profile edit 수정

--- frontend/src/components/pages/Register/Register.tsx
+++ frontend/src/components/pages/Register/Register.tsx
@@ -1,12 +1,12 @@
 import React, {useEffect, useState} from "react";
 import {useForm, SubmitHandler} from "react-hook-form";
-import Error from "components/Error/Error";
-import {Box, TextField, Button, SelectChangeEvent, FormControlLabel, Radio, RadioGroup} from "@mui/material";
-import {getCampusListQuery} from "@recoils/api/User";
-import Loading from "react-loading";
-import {useRecoilValue, useSetRecoilState} from "recoil";
-
 import {useNavigate} from "react-router-dom";
+import {useRecoilValue, useSetRecoilState} from "recoil";
+import Loading from "react-loading";
+import {Box, TextField, Button, SelectChangeEvent, FormControlLabel, Radio, RadioGroup} from "@mui/material";
+
+import {getCampusListQuery} from "@recoils/api/User";
+import Error from "components/Error/Error";
 import {postLogout, postUserRegistAxios} from "@recoils/user/axios";
 import {userGoogleAuthState} from "@recoils/Login/state";
 import {postUser} from "@recoils/types";
@@ -42,7 +42,7 @@ const Register: React.FC = () => {
   const googleAuth = useRecoilValue(userGoogleAuthState);
   const navigate = useNavigate();
 
-  const onChangeCampus = (event: SelectChangeEvent<never[]>) => {
+  const onChangeCampus = () => {
     setOpen(true);
   };
   const handleCampus = (campus: any) => {
@@ -50,18 +50,9 @@ const Register: React.FC = () => {
     setCampusSelected([campus?.name]);
     setCampusIdSelected([campus?.campusid]);
   };
-  const handleCampusReceive = (event: SelectChangeEvent<never[]>) => {
-    const selectedNames = event.target.value as string[];
-    const selectedIds = selectedNames.map(name => {
-      const campus: campusType = campusList.find(campus => campus.name === name) || {campusid: "", name: "", areaid: "", useyn: "", createdate: ""};
-      return campus ? campus.campusid : "0";
-    });
-    setCampusSelected(selectedNames);
-    setCampusIdSelected(selectedIds);
-  };
+
   const handleGednerReceive = (event: SelectChangeEvent<never>) => {
     const value = event.target.value;
-
     console.log("gender : ", value);
     setGenderSelected(value);
   };
@@ -71,10 +62,7 @@ const Register: React.FC = () => {
     setCccYNSelected(value);
   };
   const writeRegister: SubmitHandler<FormData> = async (params: FormData) => {
-    // params.list = selected;
-    // console.log(googleAuth);
     const {auth} = googleAuth;
-    // const auth = {ssoid: "test", email: "test", type: "test"};
     const userRegistInfo: postUser = {
       nickname: params.name,
       gender: genderSelected || "",
@@ -109,88 +97,62 @@ const Register: React.FC = () => {
   useEffect(() => {
     fetchData();
   }, [data]);
+
   return (
     <Box
       component="form"
       onSubmit={handleSubmit(writeRegister)}
       sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "left",
         ".row": {display: "flex", alignItems: "center", marginTop: "5px"},
         ".header": {width: "80px", textAlign: "right", paddingRight: "10px", fontSize: "20px"},
+        ".value": {minWidth: "200px"},
       }}>
       <Box className="row">
         <Box className="header">이름</Box>
-        <Box>
+        <Box className="value">
           <TextField {...register("name")} />
         </Box>
       </Box>
       {/*단일만 선택 가능하도록 일단 마무리 */}
       <Box className="row">
         <Box className="header">캠퍼스</Box>
-        <Box sx={{display: "flex", alignItems: "center", gap: 1}}>
+        <Box className="value" sx={{display: "flex", alignItems: "center", gap: 1}}>
           <Button variant="outlined" onClick={onChangeCampus}>
             캠퍼스 선택
           </Button>
           <Box>{...campusSelected}</Box>
           <CampusDialog open={open} setOpen={setOpen} items={campusList} campusSelected={campus} handleCampus={handleCampus} />
         </Box>
-        {/* <Box sx={{width: "calc(100% - 80px)", paddingRight: "10px"}}>
-          <Select
-            {...register("campusid")}
-            value={campusSelected as never}
-            fullWidth
-            multiple
-            onChange={handleCampusReceive}
-            renderValue={selected => selected.join(", ")}>
-            {campusList.map(({name, campusid}: any, index: number) => (
-              <MenuItem key={index} value={name}>
-                <Checkbox checked={campusSelected.indexOf(name.toString()) > -1} />
-                <ListItemText primary={name} />
-              </MenuItem>
-            ))}
-          </Select>
-        </Box> */}
       </Box>
       <Box className="row">
-        {" "}
         <Box className="header">학번</Box>
-        <Box>
+        <Box className="value">
           <TextField {...register("sid")} />
         </Box>
       </Box>
       <Box className="row">
-        {" "}
         <Box className="header">학과</Box>
-        <Box>
+        <Box className="value">
           <TextField {...register("major")} />
         </Box>
       </Box>
       <Box className="row">
-        {" "}
         <Box className="header">ccc 여부</Box>
-        <Box>
-          <RadioGroup
-            // {...register("cccYN")}
-            row
-            aria-labelledby="demo-radio-buttons-group-label"
-            // defaultValue="Y"
-            // name="radio-buttons-group"
-            value={(cccYNSelected as never) || null}
-            onChange={handleCCCYNReceive}>
+        <Box className="value">
+          <RadioGroup row value={(cccYNSelected as never) || null} onChange={handleCCCYNReceive}>
             <FormControlLabel value="Y" control={<Radio />} label="Y" />
             <FormControlLabel value="N" control={<Radio />} label="N" />
           </RadioGroup>
         </Box>
       </Box>
       <Box className="row">
-        {" "}
         <Box className="header">성별</Box>
-        <Box>
-          <RadioGroup
-            // {...register("gender")}
-            row
-            aria-labelledby="demo-radio-buttons-group-label"
-            value={(genderSelected as never) || null}
-            onChange={handleGednerReceive}>
+        <Box className="value">
+          <RadioGroup row aria-labelledby="demo-radio-buttons-group-label" value={(genderSelected as never) || null} onChange={handleGednerReceive}>
             <FormControlLabel value="female" control={<Radio />} label="female" />
             <FormControlLabel value="male" control={<Radio />} label="male" />
           </RadioGroup>
