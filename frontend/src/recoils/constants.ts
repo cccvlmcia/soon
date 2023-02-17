22ebd7b684d2024aac3adc85a802a1a83c3d89c6
close852 <jiwoo.choi@kccc.org>
2023-02-17 16:03:10 +0900

    토큰 처리 마저 수정

--- frontend/src/recoils/constants.ts
+++ frontend/src/recoils/constants.ts
@@ -41,4 +41,3 @@ export const options = {
     console.log("onError >> ", error.message);
   },
 };
-
--- frontend/src/recoils/user/state.tsx
+++ frontend/src/recoils/user/state.tsx
@@ -1,35 +1,17 @@
 import {atom, selector} from "recoil";
 import {axiosProcess, server} from "@recoils/constants";
-export const userSelector = selector({
+export const userSelector: any = selector({
   key: "userSelector",
-  get: async ({get}) => {
-    const user = get(userState);
+  get: async () => {
     //접속중일때, 갱신하는 상황인지, 토큰만료 후 로그인 하는 상황인지 판단
-    return await axiosProcess(
-      async () => {
-        const {data} = await server.get("/auth/user");
-        return data;
-      },
-      user ? true : false,
-    );
+    return await axiosProcess(async () => {
+      const {data} = await server.get("/auth/user");
+      return data;
+    }, false);
   },
 });
 
 export const userState = atom({
   key: "userState",
-  default: null,
+  default: userSelector,
 });
-
-// export const userSelector = selector({
-//   key: "userSelector",
-//   get: async ({get}) => {
-//     const user = get(userState);
-//     return await axiosProcess(
-//       async () => {
-//         const {data} = await server.get("/auth/user");
-//         return data;
-//       },
-//       user ? true : false,
-//     );
-//   },
-// });
