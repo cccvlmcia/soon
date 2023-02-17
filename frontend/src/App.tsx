7e066a7900c667a60657851194050eb375ae9af6
close852 <jiwoo.choi@kccc.org>
2023-02-17 12:03:42 +0900

    soon/card 호출 url 변경

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -68,7 +68,7 @@ function AppRoutes() {
               <PrevHeader />
             </Layout>
           }>
-          <Route path="/soon/:userid/card" element={Auth(SoonCard, true, loginUser || authUser)}></Route>
+          <Route path="/soon/card/:userid" element={Auth(SoonCard, true, loginUser || authUser)}></Route>
 
           <Route path="/history/:historyid/view" element={Auth(HistoryContents, true, loginUser || authUser)}></Route>
         </Route>
