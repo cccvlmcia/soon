19395940074755d607ba0bfe672078f38fe0b202
close852 <jiwoo.choi@kccc.org>
2023-02-19 19:34:47 +0900

    HistyoryEdit를 Dialolg로 변경, HistoryEdit 삭제

--- frontend/src/components/pages/History/modal/HistoryEditDialog.tsx
+++ frontend/src/components/pages/History/modal/HistoryEditDialog.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState, useRef, forwardRef} from "react";
+import React, {ChangeEvent, useState, useRef, forwardRef} from "react";
 import {Box, TextField, Select, MenuItem, Button, Checkbox, ListItemText, AppBar, IconButton, Toolbar, Typography} from "@mui/material";
 import {useForm, SubmitHandler} from "react-hook-form";
 import {useLocation, useNavigate, useParams} from "react-router-dom";
@@ -19,11 +19,15 @@ import {categoryState} from "@recoils/history/state";
 import {HistoryForm, Prayer, User} from "@recoils/types";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import {getSoonHistoryQuery} from "@recoils/soon/query";
-import {putSoonHistory} from "@recoils/history/axios";
+import {deleteSoonPray, postSoonPrays, putSoonHistory} from "@recoils/history/axios";
 import {TransitionProps} from "@mui/material/transitions";
 import Slide from "@mui/material/Slide";
 import Dialog from "@mui/material/Dialog";
 import CloseIcon from "@mui/icons-material/Close";
+import {FormControlLabel} from "@mui/material";
+import RemoveCircleOutlineIcon from "@mui/icons-material/RemoveCircleOutline";
+import {List, ListItem, ListItemButton} from "@mui/material";
+import NoData from "@common/NoData";
 
 const Transition = React.forwardRef(function Transition(
   props: TransitionProps & {
@@ -35,8 +39,6 @@ const Transition = React.forwardRef(function Transition(
 });
 
 export default function HistoryEditDialog({historyid, editMode, handleEditMode, data}: any) {
-  // const {isLoading, isError, data, error} = getSoonHistoryQuery(Number(historyid));
-
   const ref = useRef(null);
   const campusRef = useRef(null);
   const loginUser: any = useRecoilValue(userState);
@@ -44,6 +46,7 @@ export default function HistoryEditDialog({historyid, editMode, handleEditMode, 
   const [campusList, setCampusList] = useState([]);
   const [campus, setCampus]: any = useRecoilState(selectedCampusState);
   const [open, setOpen] = useState(false);
+
   //forwarRef로 초기화 하는거 만들어서, 안에서 클릭할 버튼을 만들고, 실제 이벤트는 initRef/fowardRef  , init 이벤트 초기화 ㄱㄱ
   const SubmitButton = forwardRef((props: any, ref: any) => {
     return (
@@ -119,6 +122,9 @@ function HistoryContents({historyid, SubmitButton, campus, data, handleClose}: a
   const category = categoryList?.find((c: any) => c?.id == data?.kind) || {id: "soon", name: "순모임"};
   const [categorySelected, setCategorySelected]: any = useState(category);
 
+  const [prayList, setPrayLIst] = useState<Prayer[]>(data?.prays);
+  const [prayers, setPrayers] = useState<Prayer[]>([]);
+
   const handleDateChange = (newValue: Dayjs | null) => setDate(newValue);
 
   const handleCategoryReceive = (event: any) => {
@@ -151,6 +157,69 @@ function HistoryContents({historyid, SubmitButton, campus, data, handleClose}: a
     }
     // */
   };
+
+  const handleAddPrayerField = () => {
+    console.log("handleAddPrayerField >");
+    setPrayers([...prayers, {pray: "", publicyn: "Y"}]);
+  };
+  const handlePraysSave = async () => {
+    console.log("handlePraysSave >", historyid, prayers);
+    const {data} = await postSoonPrays(historyid, prayers);
+    console.log("result >>", data);
+    if (data?.length > 0) {
+      alert("기도제목이 추가되었습니다.");
+      console.log(">>>>>>>>>>", [...prayList, ...data]);
+      setPrayLIst([...prayList, ...data]);
+      setPrayers([]);
+    }
+  };
+
+  const handlePrayerFieldChange = (event: ChangeEvent<HTMLInputElement>, index: number) => {
+    const newValues = [...prayers];
+    newValues[index].pray = event.target.value;
+    setPrayers(newValues);
+    setValue("prays", newValues); // Update the value of the 'prays' field in the form data object
+  };
+  const handlPublicynChange = (event: ChangeEvent<HTMLInputElement>, index: number) => {
+    const newValues = [...prayers];
+    newValues[index].publicyn = event.target.checked ? "Y" : "N"; // Set value to "true" or "false" as a string
+    setPrayers(newValues);
+    setValue("prays", newValues); // Update the value of the 'prays' field in the form data object
+  };
+  const handleDeletePrayerField = (index: number) => {
+    const newValues = [...prayers];
+    newValues.splice(index, 1);
+    setPrayers(newValues);
+    setValue("prays", newValues);
+  };
+  const handlePrayRemove = async (prayid: number) => {
+    const {data} = await deleteSoonPray(prayid);
+    if (data?.affected > 0) {
+      alert(" 기도가 삭제 되었습니다!");
+      const idx = prayList?.findIndex((item: any) => item?.prayid == prayid);
+      if (idx > -1) {
+        setPrayLIst([...prayList.slice(0, idx), ...prayList.slice(idx + 1, prayList?.length)]);
+      }
+    }
+  };
+
+  // const prayList = data?.prays;
+  console.log("prayList >", prayList);
+  const prayView = prayList?.map(({prayid, pray, publicyn}: any) => (
+    <ListItemButton dense={true} key={prayid} sx={{display: "flex"}}>
+      <ListItem>
+        <ListItemText>
+          {publicyn == "N" && "[비공개] "}
+          {pray}
+        </ListItemText>
+        {
+          <IconButton onClick={() => handlePrayRemove(prayid)}>
+            <RemoveCircleOutlineIcon />
+          </IconButton>
+        }
+      </ListItem>
+    </ListItemButton>
+  ));
 
   return (
     <>
@@ -237,6 +306,55 @@ function HistoryContents({historyid, SubmitButton, campus, data, handleClose}: a
           </Box>
         </Box>
         {/* 기도 추가?! */}
+        <Box className="row" sx={{flexDirection: "column", marginTop: "0!important"}}>
+          <Box sx={{background: "#F7F9FA", fontWeight: "700", width: "100%!important"}}>
+            <Box sx={{padding: "10px 20px"}}>기도제목</Box>
+          </Box>
+          {prayList?.length > 0 && (
+            <Box sx={{width: "100%"}}>
+              <List>{prayView}</List>
+            </Box>
+          )}
+          {prayList?.length == 0 && <NoData />}
+        </Box>
+        <Box className="row">
+          <Box className="value">
+            {prayers.map((value, index) => (
+              <Box key={index} sx={{display: "flex", alignItems: "center", padding: "2px 0"}}>
+                <TextField
+                  size="small"
+                  fullWidth
+                  value={value.pray}
+                  onChange={(event: ChangeEvent<HTMLInputElement>) => handlePrayerFieldChange(event, index)}
+                  required={true}
+                />
+                <Box sx={{display: "flex", alignItems: "center", minWidth: "105px", marginLeft: "10px"}}>
+                  <FormControlLabel
+                    sx={{minWidth: "75px"}}
+                    control={
+                      <Checkbox
+                        checked={value.publicyn === "Y" ? true : false}
+                        onChange={event => handlPublicynChange(event, index)}
+                        name={`publicyn-${index}`}
+                      />
+                    }
+                    label="공개"
+                  />
+                  <RemoveCircleOutlineIcon onClick={() => handleDeletePrayerField(index)} />
+                </Box>
+              </Box>
+            ))}
+          </Box>
+        </Box>{" "}
+        {/* 추가 버튼 누르면,  */}
+        <Box sx={{gap: 1}}>
+          <Button variant="outlined" fullWidth onClick={handleAddPrayerField}>
+            기도제목 추가
+          </Button>
+          <Button variant="contained" fullWidth onClick={handlePraysSave}>
+            기도제목 저장
+          </Button>
+        </Box>
         {SubmitButton}
       </Box>
     </>
