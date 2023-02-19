d7aad9bfa051b88020220ef3b618da1b6ba5282c
close852 <jiwoo.choi@kccc.org>
2023-02-19 22:37:31 +0900

    캠퍼스 요청 테이블 생성 및 서버 기능 추가

--- server/src/routes/api/campus/index.ts
+++ server/src/routes/api/campus/index.ts
@@ -1,5 +1,6 @@
 import {getCampusList} from "@campus/service/CampusService";
-import {getCampusSJList, getCampusUser} from "@user/service/UserCampusService";
+import {signUserCampusRequest} from "@user/service/UserCampusRequestService";
+import {getCampusUser} from "@user/service/UserCampusService";
 import {FastifyReply, FastifyInstance, FastifyRequest} from "fastify";
 export default async function (fastify: FastifyInstance) {
   //캠퍼스 전체 조회 말고 검색으로?만 조회?
@@ -12,5 +13,12 @@ export default async function (fastify: FastifyInstance) {
     const {campusid} = req.params;
     const soonList = await getCampusUser(campusid);
     reply.send(soonList);
+  });
+
+  //사용자 캠퍼스 요청 승인
+  fastify.post("/req/:reqid/sign", async (req: FastifyRequest<{Params: {reqid: number}}>, reply: FastifyReply) => {
+    const {reqid} = req.params;
+    const user = await signUserCampusRequest(reqid);
+    reply.send(user);
   });
 }
